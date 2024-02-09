70f49555ed526758bdcd7dfae2cfc7e4b9e06e3e
ritu-477 <ritujoharbishnoi@gmail.com>
2024-02-09 14:30:39 +0530

    digiency-homepage layout-section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html",
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"work sans"', 'sans serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
